ef3303f1891544a3d861ad9e39b0ea549c1aa27e
Kriss Wiggins <islifepeachy@outlook.com>
2022-04-15 23:08:13 +0100

    Add JSON preset configuration, migrate to CMake, add (un)installation support

--- README.md
+++ README.md
@@ -2,34 +2,47 @@
 
 Display lines of color in a true color terminal.
 
-Inspired by [cli-pride-flags](https://github.com/ExperiBass/cli-pride-flags).
-
-Implemented in POSIX C99 with no external libraries.
-
 ## Requirements
 
 - POSIX-compliant operating system
 - Terminal with true color support
 
-## Build Prerequisites
+## Build Requireements
 
 - `gcc`
-- `make`
+- `cmake` >= 3.1
+- `cjson`
 
 ## Building
 
-`make`
+`(mkdir -p build && cd build && cmake .. && make)`
+
+## Installation (as root)
+
+`(cd build && make install)`
+
+## Uninstallation
+
+`(cd build && make uninstall)`
 
 ## Usage
 
-`./chromashell -h`
+See `chromashell -h`
 
-### Example: Display a Transgender Flag
+### Example: Display a transgender flag
 
-`./chromashell -s 7ACBF5,2 -s EAACB8,2 -s FFFFFF,2 -s EAACB8,2 -s 7ACBF5,2`
+`chromashell -p trans`
+
+Or:
+
+`chromashell -s 7ACBF5,2 -s EAACB8,2 -s FFFFFF,2 -s EAACB8,2 -s 7ACBF5,2`
 
 ## License
 
 Released under the GNU GPLv3.
 
 See `LICENSE` for more information.
+
+## Acknowledgements
+
+Inspired by [cli-pride-flags](https://github.com/ExperiBass/cli-pride-flags) <3
--- chromashell.c
+++ chromashell.c
@@ -4,19 +4,26 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <sys/ioctl.h>
+#include <cjson/cJSON.h>
+
 #include "chromashell.h"
-#include "presets.h"
 
 int main(int argc, char **argv)
 {
     const char *basename = (const char *) argv[0];
+    
     winsize winsz;
     ioctl(0, TIOCGWINSZ, &winsz);
 
-    bool arg_parsed = false;
+    char *presets_path = NULL;
+    SegmentGroup *presets = NULL;
+    int n_presets = 0;
+
     Segment *segments = NULL;
     unsigned int n_segments = 0;
     
+    bool arg_parsed = false;
+
     for (int arg_index = 1; arg_index < argc; ++arg_index)
     {
         const char *arg = argv[arg_index];
@@ -28,20 +35,12 @@ int main(int argc, char **argv)
                 int n_optargs = split_optargs(&optargs, argv[arg_index], ",", 2);
                 if (n_optargs >= 2)
                 {
+                    Segment segment;
                     char *s_color = optargs[0];
                     char *s_height = optargs[1];
-                    if (strlen(s_color) == 6 && is_hex(s_color) && is_uint(s_height))
+                    if (hex_string_to_color(s_color, &segment.color, false) == 0 && is_uint(s_height))
                     {
-                        Segment segment;
-                        char s_red[3], s_green[3], s_blue[3];
-                        sprintf(s_red, "%c%c", s_color[0], s_color[1]);
-                        sprintf(s_green, "%c%c", s_color[2], s_color[3]);
-                        sprintf(s_blue, "%c%c", s_color[4], s_color[5]);
-                        segment.red = (unsigned int) strtol(s_red, NULL, 16);
-                        segment.green = (unsigned int) strtol(s_green, NULL, 16);
-                        segment.blue = (unsigned int) strtol(s_blue, NULL, 16);
                         segment.height = (unsigned int) atoi(s_height);
-
                         segments = realloc(segments, sizeof(Segment) * ++n_segments);
                         if (segments != NULL)
                         {
@@ -76,27 +75,49 @@ int main(int argc, char **argv)
         {
             if (++arg_index < argc)
             {
+                if (!presets)
+                {
+                    if (!presets_path)
+                    {
+                        presets_path = DEFAULT_PRESET_PATH;
+                    }
+
+                    FILE *file;
+                    if (file = fopen(presets_path, "r"))
+                    {
+                        n_presets = load_presets(&presets, file);
+                        if (n_presets < 0)
+                        {
+                            return err_parse_json(basename, presets_path);
+                        }
+                    }
+                    else
+                    {
+                        return err_no_file(basename, presets_path);
+                    }
+                }
+
                 char *optarg = argv[arg_index];
                 if (strcmp(optarg, "list") == 0)
                 {
-                    display_presets(&presets);
+                    display_presets(presets, n_presets);
                     return EXIT_SUCCESS;
                 }
                 else
                 {
                     bool found_preset = false;
-                    for (int preset_i = 0; preset_i < presets.count; ++preset_i)
+                    for (int preset_i = 0; preset_i < n_presets; ++preset_i)
                     {
-                        const SegmentGroup *preset = presets.groups[preset_i];
-                        if (strcmp(optarg, preset->name) == 0)
+                        const SegmentGroup preset = presets[preset_i];
+                        if (strcmp(optarg, preset.name) == 0)
                         {
                             found_preset = true;
-                            for (int segment_i = 0; segment_i < preset->n_segments; ++segment_i)
+                            for (int segment_i = 0; segment_i < preset.length; ++segment_i)
                             {
                                 segments = realloc(segments, sizeof(Segment) * ++n_segments);
                                 if (segments != NULL)
                                 {
-                                    memcpy(&segments[n_segments - 1], &preset->segments[segment_i], sizeof(Segment));
+                                    segments[n_segments - 1] = preset.segments[segment_i];
                                 }
                                 else
                                 {
@@ -117,6 +138,13 @@ int main(int argc, char **argv)
             else
             {
                 return err_no_arg(basename, arg);
+            }
+        }
+        else if (strcmp(arg, "-c") == 0 || strcmp(arg, "--config") == 0)
+        {
+            if (++arg_index < argc)
+            {
+                presets_path = argv[arg_index];
             }
         }
         else if (strcmp(arg, "-h") == 0 || strcmp(arg, "--help") == 0)
@@ -208,7 +236,10 @@ bool is_uint(const char *s)
 
 void print_segment(winsize *winsz, Segment *segment)
 {
-    printf("\033[38;2;%i;%i;%im", segment->red, segment->green, segment->blue);
+    printf(
+        "\033[38;2;%i;%i;%im",
+        segment->color.red, segment->color.green, segment->color.blue);
+
     for (int line = 0; line < segment->height; ++line)
     {
         for (int col = 0; col < winsz->ws_col; ++col)
@@ -220,13 +251,234 @@ void print_segment(winsize *winsz, Segment *segment)
     printf("\n\033[0m");
 }
 
-void display_presets(SegmentPresets *presets)
+int hex_string_to_color(const char *str, Color *color, bool has_prefix)
+{
+    if (has_prefix)
+    {
+        int input_len = strlen(str);
+        if (input_len == 7 && str[0] == '#')
+        {
+            char substr[input_len - 1];
+            strcpy(substr, &str[1]);
+            return __hex_string_to_color(substr, color);
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        return __hex_string_to_color(str, color);
+    }
+}
+
+int __hex_string_to_color(const char *str, Color *color)
+{
+    if (strlen(str) == 6 && is_hex(str))
+    {
+        char s_red[3], s_green[3], s_blue[3];
+        sprintf(s_red, "%c%c", str[0], str[1]);
+        sprintf(s_green, "%c%c", str[2], str[3]);
+        sprintf(s_blue, "%c%c", str[4], str[5]);
+        color->red = (unsigned int) strtol(s_red, NULL, 16);
+        color->green = (unsigned int) strtol(s_green, NULL, 16);
+        color->blue = (unsigned int) strtol(s_blue, NULL, 16);
+    }
+    else
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+int load_presets(SegmentGroup **dest, FILE *file)
+{
+    char *json_str = NULL;
+    char buf[100];
+    int json_strlen = 0;
+    while (fscanf(file, "%s", buf) != EOF)
+    {
+        json_strlen += strlen(buf);
+        json_str = realloc(json_str, (json_strlen * sizeof(char)) + 1);
+
+        if (json_str != NULL)
+        {
+            strcat(json_str, buf);
+        }
+        else
+        {
+            exit(err_alloc());
+        }
+    }
+    
+    if (json_str != NULL)
+    {
+        const cJSON *root = cJSON_ParseWithLength(json_str, json_strlen);
+        const cJSON *item = NULL;
+        int parse_err = 0;
+        SegmentGroup *presets = NULL;
+        unsigned int n_presets = 0;
+        unsigned int presets_sz = 0;
+        if (cJSON_IsArray(root))
+        {
+            cJSON_ArrayForEach(item, root)
+            {
+                SegmentGroup preset;
+
+                const cJSON *item_name = cJSON_GetObjectItem(item, JSON_PRESET_NAME);
+                if (item_name != NULL)
+                {
+                    if (cJSON_IsString(item_name))
+                    {
+                        preset.name = cJSON_GetStringValue(item_name);
+                    }
+                    else
+                    {
+                        parse_err = err_json_not_string(JSON_PRESET_NAME);
+                        break;
+                    }
+                }
+                else
+                {
+                    parse_err = err_json_no_object(JSON_PRESET_NAME);
+                    break;
+                }
+                
+                cJSON *item_segments = cJSON_GetObjectItem(item, JSON_PRESET_SEGMENTS);
+                if (item_segments != NULL)
+                {
+                    if (cJSON_IsArray(item_segments))
+                    {
+                        const cJSON *item_segment;
+                        Segment *segments = NULL;
+                        unsigned int n_segments = 0;
+                        cJSON_ArrayForEach(item_segment, item_segments)
+                        {
+                            Segment segment;
+
+                            cJSON *segment_color = cJSON_GetObjectItem(item_segment, JSON_PRESET_COLOR);
+                            if (segment_color != NULL)
+                            {
+                                if (cJSON_IsString(segment_color))
+                                {
+                                    const char *color_str = cJSON_GetStringValue(segment_color);
+                                    Color color;
+                                    int color_parse_err = hex_string_to_color(color_str, &color, true);
+                                    if (color_parse_err >= 0)
+                                    {
+                                        segment.color = color;
+                                    }
+                                    else
+                                    {
+                                        parse_err = err_json_parse_color(color_str);
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    parse_err = err_json_not_string(JSON_PRESET_COLOR);
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                parse_err = err_json_no_object(JSON_PRESET_COLOR);
+                                break;
+                            }
+
+                            cJSON *segment_height = cJSON_GetObjectItem(item_segment, JSON_PRESET_HEIGHT);
+                            if (segment_height != NULL)
+                            {
+                                if (cJSON_IsNumber(segment_height))
+                                {
+                                    double segment_height_d = cJSON_GetNumberValue(segment_height);
+                                    if (segment_height_d == (int) segment_height_d)
+                                    {
+                                        segment.height = (int) segment_height_d;
+                                    }
+                                    else
+                                    {
+                                        parse_err = err_json_not_int(JSON_PRESET_HEIGHT);
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    parse_err = err_json_not_number(JSON_PRESET_HEIGHT);
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                parse_err = err_json_no_object(JSON_PRESET_HEIGHT);
+                                break;
+                            }
+
+                            segments = realloc(segments, sizeof(Segment) * ++n_segments);
+                            segments[n_segments - 1] = segment;
+                        }
+
+                        unsigned int segments_sz = n_segments * sizeof(Segment);
+                        preset.segments = malloc(segments_sz);
+                        memcpy(preset.segments, segments, segments_sz);
+                        preset.length = n_segments;
+                    }
+                    else
+                    {
+                        parse_err = err_json_not_array(JSON_PRESET_SEGMENTS);
+                        break;
+                    }
+                }
+                else
+                {
+                    parse_err = err_json_no_object(JSON_PRESET_SEGMENTS);
+                    break;
+                }
+
+                presets_sz += sizeof(SegmentGroup);
+                presets = realloc(presets, presets_sz);
+                if (presets != NULL)
+                {
+                    presets[n_presets] = preset;
+                    ++n_presets;
+                }
+                else
+                {
+                    exit(err_alloc());
+                }
+            }
+        }
+        else
+        {
+            parse_err = err_json_root_not_array();
+        }
+
+        free(json_str);
+
+        if (parse_err != 0)
+        {
+            return -1;
+        }
+        else if (presets != NULL)
+        {
+            *dest = presets;
+        }
+
+        return n_presets;
+    }
+
+    return -1;
+}
+
+void display_presets(SegmentGroup *presets, unsigned int n_presets)
 {
     printf("Available presets:\n ");
-    for (int preset_i = 0; preset_i < presets->count; ++preset_i)
+    for (int i = 0; i < n_presets; ++i)
     {
-        printf("%s", presets->groups[preset_i]->name);
-        if (preset_i < presets->count - 1)
+        printf("%s", presets[i].name);
+        if (i < n_presets - 1)
         {
             printf(" ");
         }
@@ -240,8 +492,9 @@ void display_help(const char *basename)
         "Usage: %s OPTIONS ...\n"
         "Display lines of color in a true color terminal.\n"
         " -s,   --segment RRGGBB,HEIGHT         specify segment hex color and height in lines\n"
-        " -p,   --preset NAME                   display preset segment configuration\n"
+        " -p,   --preset NAME                   display preset\n"
         "                                         use '--preset list' to display available preset names\n"
+        " -c,   --config FILE                   specify path of preset configuration JSON\n"
         " -h,   --help                          display this help and exit\n"
         "       --version                       display version information and exit\n",
         basename);
@@ -251,7 +504,7 @@ void display_version(void)
 {
     printf(
         "ChromaShell %s\n"
-        "Copyright (C) %s Adora Zero.\n"
+        "Copyright (c) %s Adora Zero.\n"
         "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n"
         "This is free software: you are free to change and redistribute it.\n"
         "There is NO WARRANTY, to the extent permitted by law.\n\n"
@@ -271,7 +524,6 @@ int err_no_arg(const char *basename, const char *opt)
         "%s: missing required argument for '%s'\n"
         "Try '%s --help' for more information.\n",
         basename, opt, basename);
-
     return EXIT_FAILURE;
 }
 
@@ -282,7 +534,6 @@ int err_no_preset(const char *basename, const char *optarg)
         "Try '%s --preset list' to display available preset names,"
         " or '%s --help' for more information.\n",
         basename, optarg, basename, basename);
-
     return EXIT_FAILURE;
 }
 
@@ -292,6 +543,84 @@ int err_no_opt(const char *basename, const char *opt)
         "%s: unrecognized option '%s'\n"
         "Try '%s --help' for more information.\n",
         basename, opt, basename);
-
     return EXIT_FAILURE;
+}
+
+int err_parse_color(const char *basename, const char *str)
+{
+    printf(
+        "%s: invalid hexadecimal color '%s'\n"
+        "Try '%s --help' for more information.\n",
+        basename, str, basename);
+    return EXIT_FAILURE;
+}
+
+int err_parse_json(const char *basename, const char *filename)
+{
+    printf(
+        "%s: error parsing JSON file '%s'\n",
+        basename, filename);
+    return EXIT_FAILURE;
+}
+
+int err_no_file(const char *basename, const char *filename)
+{
+    printf(
+        "%s: file '%s' not found\n",
+        basename, filename);
+    return EXIT_FAILURE;
+}
+
+int err_json_root_not_array(void)
+{
+    printf("JSON error: root object is not an array\n");
+    return -1;
+}
+
+int err_json_no_object(const char *name)
+{
+    printf(
+        "JSON error: object '%s' not found\n",
+        name);
+    return -1;
+}
+
+int err_json_not_string(const char *name)
+{
+    printf(
+        "JSON error: object '%s' is not a string\n",
+        name);
+    return -1;
+}
+
+int err_json_not_int(const char *name)
+{
+    printf(
+        "JSON error: object '%s' is not an integer\n",
+        name);
+    return -1;
+}
+
+int err_json_not_number(const char *name)
+{
+    printf(
+        "JSON error: object '%s' is not a number\n",
+        name);
+    return -1;
+}
+
+int err_json_not_array(const char *name)
+{
+    printf(
+        "JSON error: object '%s' is not an array\n",
+        name);
+    return -1;
+}
+
+int err_json_parse_color(const char *str)
+{
+    printf(
+        "JSON error: invalid hexadecimal color '%s'\n",
+        str);
+    return -1;
 }
